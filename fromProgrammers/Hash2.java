1c3f953cabcbfca6bd25ec3bde4c9d2ed2dbf9bb
LanCo_Jang <cyj9212@gmail.com>
2019-01-16 00:10:27 +0900

    hash2 문제 완료(전화번호 목록)

--- fromProgrammers/Hash2.java
+++ fromProgrammers/Hash2.java
@@ -1,8 +1,34 @@
 package fromProgrammers;
 
- /*
+import java.util.HashMap;
+
+/*
   * problem URL: https://programmers.co.kr/learn/courses/30/lessons/42577
   * */
 public class Hash2 {
-
+	public static boolean solution(String[] p) {
+		Boolean answer = true;
+		for(int i=0;i<p.length;i++) {
+			for(int j=0;j<p.length;j++) {
+				if(i==j) {
+					continue;
+				} else {
+					if(p[i].indexOf(p[j])==0) {
+						answer = false;
+						break;
+					}
+				}
+			}
+		}
+		return answer;
+	}
+	
+	public static void main(String args[]) {
+		String[] a = {"12232332", "12", "222222"};
+		String[] b = {"123", "456", "789"};
+		String[] c = {"12", "123", "1235", "567", "88"};
+		
+		Boolean answer = solution(a);
+		System.out.println(answer);
+	}
 }
