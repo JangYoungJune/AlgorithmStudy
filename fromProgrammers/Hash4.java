f83e24aefb7e00575fe7edcdaa63882a215d3c03
LanCo_Jang <cyj9212@gmail.com>
2019-01-25 01:42:18 +0900

    Hash4문제 풀이(베스트앨범)

--- fromProgrammers/Hash4.java
+++ fromProgrammers/Hash4.java
@@ -1,34 +1,102 @@
 package fromProgrammers;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 /*
   * problem URL: https://programmers.co.kr/learn/courses/30/lessons/42578
   * */
 public class Hash4 {
     public static int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
         HashMap<String, HashMap<Integer, Integer>> musicMap = new HashMap<>();
+        HashMap<String, Integer> genreTotalPlays = new HashMap<>();
+        List<Integer> answerList = new ArrayList<>();
         for(int i=0;i<genres.length;i++) {
         	if(musicMap.containsKey(genres[i])) {
+        		genreTotalPlays.put(genres[i], genreTotalPlays.get(genres[i])+plays[i]);
         		if(musicMap.get(genres[i]).size()==2) {
-        			// 각 두개의 값을 받아서 크기를 확인한 후, 작은값은 버리고, 이값을 추가한다.
+        			int maxKey = -1, maxValue = -1;
+        			for(Map.Entry<Integer, Integer> item : musicMap.get(genres[i]).entrySet()) {
+        				if(maxKey==-1) {
+        					maxKey = item.getKey();
+        					maxValue = item.getValue();
+        				} else {
+        					if(maxValue > item.getValue()) {
+        						maxKey = item.getKey();
+        					} else if(maxValue == item.getValue()) {
+        						maxKey = (maxKey < item.getKey())? item.getKey() : maxKey;
+        					}
+        				}
+        			}
+        			int compareValue = musicMap.get(genres[i]).get(maxKey);
+        			if(compareValue < plays[i]) {
+        				musicMap.get(genres[i]).remove(maxKey);
+        				musicMap.get(genres[i]).put(i, plays[i]);
+        			}
+        		} else {
+        			musicMap.get(genres[i]).put(i, plays[i]);
         		}
-        		musicMap.get(genres[i]).put(i, plays[i]);
         	} else {
+        		genreTotalPlays.put(genres[i], plays[i]);
         		musicMap.put(genres[i], new HashMap<Integer, Integer>());
         		musicMap.get(genres[i]).put(i, plays[i]);
         	}
         }
-
-        return answer;
+        List<String> list = new ArrayList<>();
+        list.addAll(genreTotalPlays.keySet());
+        Collections.sort(list, new Comparator<String>(){
+        	public int compare(String o1, String o2) {
+        		Object v1 = genreTotalPlays.get(o1);
+        		Object v2 = genreTotalPlays.get(o2);
+        		return ((Comparable)v2).compareTo(v1);
+        	}
+        });
+        Iterator it = list.iterator();
+        
+        while(it.hasNext()) {
+        	String genre = (String)it.next();
+        	HashMap<Integer,Integer> tempMusicMap = musicMap.get(genre);
+        	int readyKey = -1, readyValue = -1;
+        	for(Map.Entry<Integer, Integer> item : tempMusicMap.entrySet()) {
+            	if(readyKey == -1) {
+            		readyKey = item.getKey();
+            		readyValue = item.getValue();
+            	} else {
+            		int firstKey, secondKey;
+            		if(readyValue > item.getValue()) {
+            			firstKey = readyKey;
+            			secondKey = item.getKey();
+            		} else if(readyValue == item.getValue()) {
+            			firstKey = (readyKey>item.getKey())? item.getKey() : readyKey;
+            			secondKey = (readyKey>item.getKey())? readyKey : item.getKey();
+            		} else {
+            			firstKey = item.getKey();
+            			secondKey = readyKey;
+            		}
+            		answerList.add(firstKey);
+            		answerList.add(secondKey);
+            	}
+            }
+        	if(tempMusicMap.size()==1) {
+        		answerList.add(readyKey);
+        	}
+        }
+        return answerList.stream().mapToInt(i->i).toArray();
     }
 
     public static void main(String args[]) {
-        String[] a = { "classic", "pop", "classic", "classic", "pop" };
-        int[] b = { 500, 600, 150, 800, 2500 };
+        String[] a = {"classic", "pop", "classic", "pop", "classic", "classic"};
+        int[] b = {400, 600, 150, 2500, 500, 500};
         
         int[] answer = solution(a, b);
-        System.out.println(answer);
+        for(int i=0; i<answer.length; i++) {
+        	System.out.print(answer[i]);
+        }
+        
     }
 }
