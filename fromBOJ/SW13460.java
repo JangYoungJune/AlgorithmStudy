9665dad23f43b909a6f5d271c22434d0af07f289
LanCo_Jang <cyj9212@gmail.com>
2019-08-14 02:02:27 +0900

    2019-08-14 boj13460 구슬탈출2 해결

--- fromBOJ/SW13460.java
+++ fromBOJ/SW13460.java
@@ -48,66 +48,99 @@ public class SW13460 {
 		} else {
 			// 만약 상하나 좌우로 갈때, 공이 같이 움직일 수 있다>? 앞에 있는걸 먼저 움직임 + 다음걸 움직임
 			// 공을 움직일때, 미리 빈공간 갯수를 세어 넘겨줌 + 만약 공이 같이 움직이는 상황이면? 공 위치 전으로 옮기는거 잊지말자
-			int reverseDir = 0;
+			int reverseDir = -1;
 			if(dir!=0) {
 				reverseDir = (dir%2==0)? dir-1 : dir+1;
 			}
 			for(int i=0;i<dir4d.length;i++) {
-				boolean isPossible = false;
 				int x = dir4d[i][0];
 				int y = dir4d[i][1];
-				int multi = 1;
-				// 상하좌우에 R/B 가 붙어있을경우를 처리
-				if(map[rPos[0]+x][rPos[1]+y]==map[bPos[0]][bPos[1]]) {
-					multi++;
-				}
-				// 빈공간이 잇을경우 가능 처리 + 전방향을 돌아가는건 막자
-				
-				if(reverseDir!=i+1 && map[rPos[0]+multi*x][rPos[1]+multi*y]=='.') {
-					isPossible = true;
-				}
-				// 해당 방향으로 이동이 불가능하다고 하면 다른 방향 체크
-				if(!isPossible) {
+				// 블루 또는 레드가 해당 방향으로 이동하는데, 값이 바뀔게 없다? 그럼 그 길은 skip / 아니면 케이스 GO + 이전경로로 번복할경우
+				// 두 색 중 하나는 방향으로 1칸이라도 이동하는데 .이나 0을 만나야 한다.
+				if(dir==reverseDir || !(canMove(rPos[0]+x,rPos[1]+y) || canMove(bPos[0]+x,bPos[1]+y))) {
 					continue;
 				}
-				// 같은 라인에 있을경우를 체크
-				boolean isSameLine = true;
-				boolean moveRedFirst = true;
-				if(x==0 && rPos[0]==bPos[0]) { // 가로축
-					for(int j=Math.min(rPos[1], bPos[1]);j<=Math.max(rPos[1], bPos[1]);j++) {
-						if(map[rPos[0]][j]=='#') {
-							isSameLine = false;
-							break;
-						}
-						if(isSameLine) {
-							if(y>0) { // 왼쪽
-								moveRedFirst = (Math.min(rPos[1],bPos[1])==rPos[1])? true : false;
-							} else {
-								moveRedFirst = (Math.min(rPos[1],bPos[1])==rPos[1])? false : true;
-							}
-						}
-					}
-				} else if(y==0 && rPos[1]==bPos[1]) { // 세로축
-					for(int j=Math.min(rPos[0], bPos[0]);j<=Math.max(rPos[0], bPos[0]);j++) {
-						if(map[j][rPos[1]]=='#') {
-							isSameLine = false;
-							break;
-						}
-						if(isSameLine) {
-							if(x>0) { // 위
-								moveRedFirst = (Math.min(rPos[0],bPos[0])==rPos[0])? true : false;
-							} else {
-								moveRedFirst = (Math.min(rPos[0],bPos[0])==rPos[0])? false : true;
-							}
-						}
-					}
-				}
 				
-				// 거리를 재서 R과 B를 이동시킨다.
-				// 그다음에 방향값을 넣고 dfs 돌린다.
-				int[] firstPos = (moveRedFirst)? rPos : bPos;
-				int[] secondPos = (moveRedFirst)? bPos : rPos;
+				boolean metBlue = false;
+				boolean metRed = false;
+				boolean canRedPass = false;
+				boolean canBluePass = false;
+				int rIdx = 0;
+				int bIdx = 0;
+				// rPos와 bPos를 저장 > 새로운 위치의 rPos, bPos, map을 구성하고 dfs > 다시 돌림
+				while(map[rPos[0]+(rIdx+1)*x][rPos[1]+(rIdx+1)*y]!='#') {
+					if(map[rPos[0]+(rIdx+1)*x][rPos[1]+(rIdx+1)*y]=='B') {
+						metBlue = true;
+					}
+					if(map[rPos[0]+(rIdx+1)*x][rPos[1]+(rIdx+1)*y]=='O') {
+						canRedPass = true;
+					}
+					rIdx++;
+				}
+				while(map[bPos[0]+(bIdx+1)*x][bPos[1]+(bIdx+1)*y]!='#') {
+					if(map[bPos[0]+(bIdx+1)*x][bPos[1]+(bIdx+1)*y]=='R') {
+						metRed = true;
+					}
+					if(map[bPos[0]+(bIdx+1)*x][bPos[1]+(bIdx+1)*y]=='O') {
+						canBluePass = true;
+					}
+					bIdx++;
+				}
+				if(canBluePass || canRedPass) {
+					dfs(cnt+1,canRedPass, canBluePass, i+1);
+				} else {
+					int[] copyRPos = new int[2];
+					int[] copyBPos = new int[2];
+					for(int j=0;j<2;j++) {
+						copyRPos[j] = rPos[j];
+						copyBPos[j] = bPos[j];
+					}
+					if(metBlue) { // 가는 길목에 파란공이 있다? 기존 idx값에는 blue공을 넣어주고 이전칸에 빨간공을 이동시켜준다.
+						rPos[0] = rPos[0] + (rIdx-1)*x;
+						rPos[1] = rPos[1] + (rIdx-1)*y;
+						map[copyRPos[0]][copyRPos[1]] = '.';
+						map[rPos[0]][rPos[1]] = 'R';
+						bPos[0] = bPos[0] + bIdx*x;
+						bPos[1] = bPos[1] + bIdx*y;
+						map[copyBPos[0]][copyBPos[1]] = '.';
+						map[bPos[0]][bPos[1]] = 'B';
+					} else if(metRed){ // 빨간공을 해당 위치로 이동시킴 > while로 파란공 위치를 찾는다(R을 만나는 경우에도 멈추게 해주면 된다.) + 파란공 위치로 이동
+						rPos[0] = rPos[0] + rIdx*x;
+						rPos[1] = rPos[1] + rIdx*y;
+						map[copyRPos[0]][copyRPos[1]] = '.';
+						map[rPos[0]][rPos[1]] = 'R';
+						bPos[0] = bPos[0] + (bIdx-1)*x;
+						bPos[1] = bPos[1] + (bIdx-1)*y;
+						map[copyBPos[0]][copyBPos[1]] = '.';
+						map[bPos[0]][bPos[1]] = 'B';
+					} else {
+						rPos[0] = rPos[0] + rIdx*x;
+						rPos[1] = rPos[1] + rIdx*y;
+						map[copyRPos[0]][copyRPos[1]] = '.';
+						map[rPos[0]][rPos[1]] = 'R';
+						bPos[0] = bPos[0] + bIdx*x;
+						bPos[1] = bPos[1] + bIdx*y;
+						map[copyBPos[0]][copyBPos[1]] = '.';
+						map[bPos[0]][bPos[1]] = 'B';
+					}
+					
+					dfs(cnt+1,canRedPass, canBluePass, i+1);
+					
+					map[rPos[0]][rPos[1]] = '.';
+					map[bPos[0]][bPos[1]] = '.';
+					rPos = copyRPos;
+					bPos = copyBPos;
+					map[rPos[0]][rPos[1]] = 'R';
+					map[bPos[0]][bPos[1]] = 'B';
+				}
 			}
+		}
+	}
+	public static boolean canMove(int x, int y) {
+		if(map[x][y]=='.' || map[x][y]=='O') {
+			return true;
+		} else {
+			return false;
 		}
 	}
 	
