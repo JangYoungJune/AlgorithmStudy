ab9873a39c2619a911a0676ec6eb501f7c310989
JangYoungjune <cyj9212@gmail.com>
2019-10-24 11:11:21 +0900

    2019-10-24 알고리즘 제출

---
+++ fromSWExpertAcademy/SW6719.java
@@ -0,0 +1,29 @@
+package fromSWExpertAcademy;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class SW6719 {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int tcNum = Integer.parseInt(br.readLine().trim());
+		for(int tc=1;tc<=tcNum;tc++) {
+			st = new StringTokenizer(br.readLine().trim()," ");
+			int N = Integer.parseInt(st.nextToken());
+			int K = Integer.parseInt(st.nextToken());
+			int[] arr = new int[N];
+			st = new StringTokenizer(br.readLine().trim()," ");
+			for(int i=0;i<N;i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			Arrays.sort(arr);
+			double value = 0;
+			for(int i=N-K;i<N;i++) {
+				value = (value+(double)arr[i])/2;
+			}
+			System.out.printf("#%d %.6f\n",tc, value);
+		}
+	}
+}
