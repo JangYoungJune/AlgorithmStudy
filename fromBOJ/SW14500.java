f76d17625e4a941c751bc7455db2925e83e77e5f
JangYoungJune <cyj9212@gmail.com>
2020-01-18 13:22:02 +0900

    add problem solve-boj 14500

--- fromBOJ/SW14500.java
+++ fromBOJ/SW14500.java
@@ -1,86 +1,116 @@
 package fromBOJ;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class SW14500 {
+	static int n, m;
+	static int[][] arr;
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		st = new StringTokenizer(br.readLine().trim()," ");
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int[][] arr = new int[N][M];
-		
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine().trim()," ");
-			for (int j = 0; j < M; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
+		String[] inputStr = br.readLine().trim().split(" ");
+		n = Integer.parseInt(inputStr[0]);
+		m = Integer.parseInt(inputStr[1]);
+		arr = new int[n][m];
+
+		for (int i = 0; i < n; i++) {
+			String[] arrInt = br.readLine().trim().split(" ");
+			for (int j = 0; j < m; j++) {
+				arr[i][j] = Integer.parseInt(arrInt[j]);
 			}
 		}
-		
+
 		int max = 0;
-		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				
-				if(j+3<M) { // 우 4칸
-					int tempVal = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				int sum;
+				if (canSize(i, j, 3, 0)) { // 아래 3개
+					sum = 0;
 					for(int k=0;k<4;k++) {
-						tempVal += arr[i][j+k]; 
+						sum += arr[i+k][j];
 					}
-					max = Math.max(max, tempVal);
+					if(max<sum) {
+						max = sum;
+					}
 				}
 				
-				if(i+3<N) { // 하 4칸
-					int tempVal = 0;
+				if (canSize(i, j, 2, 1)) {
+					sum = 0;
+					for(int k=0;k<3;k++) {
+						sum += arr[i+k][j];
+					}
+					sum += Math.max(arr[i][j+1], Math.max(arr[i+1][j+1], arr[i+2][j+1]));
+					int otherSum = arr[i][j]+arr[i+1][j]+arr[i+1][j+1]+arr[i+2][j+1];
+					sum = Math.max(sum, otherSum);
+					if(max< sum) {
+						max = sum;
+					}
+				}
+				if (canSize(i, j, 2, -1)) {
+					sum = 0;
+					for(int k=0;k<3;k++) {
+						sum += arr[i+k][j];
+					}
+					sum += Math.max(arr[i][j-1], Math.max(arr[i+1][j-1], arr[i+2][j-1]));
+					int otherSum = arr[i][j]+arr[i+1][j]+arr[i+1][j-1]+arr[i+2][j-1];
+					sum = Math.max(sum, otherSum);
+					if(max<sum) {
+						max = sum;
+					}
+				}
+
+				if (canSize(i, j, 0, 3)) {
+					sum = 0;
 					for(int k=0;k<4;k++) {
-						tempVal += arr[i+k][j]; 
+						sum += arr[i][j+k];
 					}
-					max = Math.max(max, tempVal);
+					if(max<sum) {
+						max = sum;
+					}
 				}
 				
-				if(i+2<N && j+1<M) { // 하 3칸  우 1칸
-					int tempVal = 0;
+				if (canSize(i, j, 1, 2)) {
+					sum = 0;
 					for(int k=0;k<3;k++) {
-						tempVal += arr[i+k][j]; 
+						sum += arr[i][j+k];
 					}
-					max = Math.max(max, (tempVal+Math.max(arr[i+1][j+1], arr[i+2][j+1])));
-					max = Math.max(max, (tempVal-arr[i+2][j]+arr[i+1][j+1]+arr[i+2][j+1]));
+					sum += Math.max(arr[i+1][j], Math.max(arr[i+1][j+1], arr[i+1][j+2]));
+					int otherSum = arr[i][j]+arr[i][j+1]+arr[i+1][j+1]+arr[i+1][j+2];
+					sum = Math.max(sum, otherSum);
+					if(max<sum) {
+						max = sum;
+					}
 				}
-				
-				if(i+2<N && j-1>=0) { // 하 3칸  좌 1칸
-					int tempVal = 0;
+				if (canSize(i, j, -1, 2)) {
+					sum = 0;
 					for(int k=0;k<3;k++) {
-						tempVal += arr[i+k][j]; 
+						sum += arr[i][j+k];
 					}
-					max = Math.max(max, (tempVal+Math.max(arr[i][j-1], arr[i+1][j-1])));
+					sum += Math.max(arr[i-1][j], Math.max(arr[i-1][j+1], arr[i-1][j+2]));
+					int otherSum = arr[i][j]+arr[i][j+1]+arr[i-1][j+1]+arr[i-1][j+2];
+					sum = Math.max(sum, otherSum);
+					if(max<sum) {
+						max = sum;
+					}
 				}
 				
-				if(i+1<N && j+2<M) { // 우 3칸 하1칸
-					int tempVal = 0;
-					for(int k=0;k<3;k++) {
-						tempVal += arr[i][j+k]; 
+				if (canSize(i, j, 1, 1)) {
+					sum = 0;
+					sum = arr[i][j]+arr[i+1][j] + arr[i][j+1]+arr[i+1][j+1];
+					if(max<sum) {
+						max = sum;
 					}
-					max = Math.max(max, (tempVal+Math.max(arr[i+1][j], arr[i+1][j+1])));
 				}
-				
-				if(i-1>=0 && j+2<M) { // 우 3칸 상1칸
-					int tempVal = 0;
-					for(int k=0;k<3;k++) {
-						tempVal += arr[i][j+k]; 
-					}
-					max = Math.max(max, (tempVal+Math.max(arr[i-1][j+1], arr[i-1][j+2])));
-					max = Math.max(max, (tempVal-arr[i][j+2]+arr[i-1][j+1]+arr[i-1][j+2]));
-				}
-				
-				if(i+1<N && j+1<M) { // 우 1칸 하1칸
-					max = Math.max(max, (arr[i][j]+arr[i+1][j]+arr[i][j+1]+arr[i+1][j+1]));
-				}
+
 			}
 		}
-		
 		System.out.println(max);
 	}
+
+	public static boolean canSize(int x, int y, int ex, int ey) {
+		x = x + ex;
+		y = y + ey;
+		return (x >= 0 && x < n && y >= 0 && y < m);
+	}
+
 }
